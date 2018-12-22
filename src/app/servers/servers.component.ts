95f98e83f0a88026dda4dddcf4fe1671e1bfd321
RemekGdansk <Lukasz.Klonowski@gmail.com>
2018-12-22 21:15:56 +0100

    After S02L39

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus: string = 'No server was created';
   serverName: string = 'Test server';
   serverCreated: boolean = false;
+  servers: string[] = ['TestServer', 'TestServer 2'];
 
   constructor() {
     setTimeout(() => {
@@ -22,6 +23,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.servers.push(this.serverName);
     this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
